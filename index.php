aebc4e1b9b062e7d25b126ff0d20fc0d6767a7da
bimopandapotan <bimosatp522@gmail.com>
2025-07-22 09:54:18 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit maxime delectus rerum quam pariatur beatae nobis, ducimus consectetur id, molestiae quod laborum aut perferendis assumenda corrupti ab temporibus rem tempora.</p>
 </body>
 </html>
